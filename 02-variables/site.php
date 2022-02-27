4cb08c956ae62f81ce293827d4be2684991c9017
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-02-27 00:48:09 -0300

    add  02-variables/

---
+++ 02-variables/site.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Learn PHP - freeCodeCamp</title>
+    </head>
+    <body>
+        
+        <?php
+            /* 
+                *> Sintaxe: 
+                    $nomeVariavel = valor; 
+
+                *> É fracamente tipada logo não vai precisar definir o tipo;
+                *> É dinamica, ou seja, pode-string no começo e depois podendo receber integer;
+            */
+
+            $characterName = "Gustavo Henrique";
+            $characterAge = 16;
+            $characterAido = "I'am love code💜🐱‍🚀";
+
+            echo "My name is $characterName, I'am  $characterAge years old";
+            echo "<br>";
+            echo "Aido: $characterAido";
+        ?>
+
+    </body>
+</html
