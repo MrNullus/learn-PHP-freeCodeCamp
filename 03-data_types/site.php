5413b423172a781200a7f6fa82e248d828a60ba6
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-02-27 00:46:43 -0300

    add 03-data_types/

---
+++ 03-data_types/site.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Learn PHP - freeCodeCamp</title>
+    </head>
+    <body>
+        
+        <?php
+            /* 
+                *> Data Types, tipos de dados: 
+
+                    -> String == "qualquer caractere entre aspas";
+                    -> Int == todo número que não tiver (.) - ou seja sem casa decimal;
+                    -> Float == todo número decimal - ou seja - com casa decimal;
+                    -> Boolean == false ou true;
+            */
+
+            $phrase = "To be or not to be";
+            $age = 16;
+            $gpa = 30.121291;
+            $isDev = true;
+
+            echo "$phrase <br> $age <br> $gpa <br> $";
+        ?>
+
+    </body>
+</html
