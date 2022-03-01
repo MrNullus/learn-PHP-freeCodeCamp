a0d750c4f3cead3f9f90a20acf82a389880ad939
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-03-01 02:12:45 -0300

    21-include-php"

---
+++ 21-include-php/site.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Learn PHP - freeCodeCamp</title>
+    </head>
+    <body>
+
+        <?php 
+            include "article-header.php";
+
+            $title = "My First Post";
+            $author = "GaGu";
+            $wordCount = 900;
+
+
+            include "useful-tools.php";
+
+            echo $feetInMile;
+            sayHi("Gustavito");
+
+        ?>
+
+    </body>
+</html>
